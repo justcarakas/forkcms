e1d898d1eec70cfd3d337cad95c1bee108b6308b
Jelmer Prins <jelmer@sumocoders.be>
2017-12-06 14:58:01 +0100

    Value Object for the type of locale

---
+++ src/Backend/Modules/Locale/Domain/Locale/Type/Type.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Backend\Modules\Locale\Domain\Locale\Type;
+
+use InvalidArgumentException;
+
+final class Type
+{
+    private const LABEL = 'lbl';
+    private const MESSAGE = 'msg';
+    private const ACTION = 'act';
+    private const ERROR = 'err';
+    private const POSSIBLE_VALUES = [
+        self::LABEL,
+        self::MESSAGE,
+        self::ACTION,
+        self::ERROR,
+    ];
+
+    /** @var string */
+    private $type;
+
+    public function __construct(string $type)
+    {
+        if (!\in_array($type, self::POSSIBLE_VALUES, true)) {
+            throw new InvalidArgumentException('Invalid value');
+        }
+
+        $this->type = $type;
+    }
+
+    public function getValue(): string
+    {
+        return $this->type;
+    }
+
+    public function __toString(): string
+    {
+        return $this->type;
+    }
+
+    public function equals(self $type): bool
+    {
+        if (!($type instanceof $this)) {
+            return false;
+        }
+
+        return $type == $this;
+    }
+
+    public static function label(): self
+    {
+        return new self(self::LABEL);
+    }
+
+    public function isLabel(): bool
+    {
+        return $this->equals(self::label());
+    }
+
+    public static function message(): self
+    {
+        return new self(self::MESSAGE);
+    }
+
+    public function isMessage(): bool
+    {
+        return $this->equals(self::message());
+    }
+
+    public static function action(): self
+    {
+        return new self(self::ACTION);
+    }
+
+    public function isAction(): bool
+    {
+        return $this->equals(self::action());
+    }
+
+    public static function error(): self
+    {
+        return new self(self::ERROR);
+    }
+
+    public function isError(): bool
+    {
+        return $this->equals(self::error());
+    }
+}
