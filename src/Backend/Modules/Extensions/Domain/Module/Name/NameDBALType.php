948273542ac6fc7d2efc38fda1ce35c6c3bb91e4
Jelmer Prins <jelmer@sumocoders.be>
2017-12-06 18:21:18 +0100

    Add a DBALType for the value object for a module name

---
+++ src/Backend/Modules/Extensions/Domain/Module/Name/NameDBALType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Backend\Modules\Extensions\Domain\Module\Name;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\TextType;
+
+final class NameDBALType extends TextType
+{
+    public function getName(): string
+    {
+        return 'extensions_module_name';
+    }
+
+    /**
+     * @param string $name
+     * @param AbstractPlatform $platform
+     *
+     * @return Name
+     */
+    public function convertToPHPValue($name, AbstractPlatform $platform): Name
+    {
+        return new Name($name);
+    }
+
+    /**
+     * @param Name $name
+     * @param AbstractPlatform $platform
+     *
+     * @return string
+     */
+    public function convertToDatabaseValue($name, AbstractPlatform $platform): string
+    {
+        return $name->getValue();
+    }
+}
