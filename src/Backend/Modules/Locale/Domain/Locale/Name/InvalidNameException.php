5f796b55d3f1f10e49f2df824b55975df4ff230c
Jelmer Prins <jelmer@sumocoders.be>
2017-12-06 15:45:11 +0100

    Add missing return types

--- src/Backend/Modules/Locale/Domain/Locale/Name/InvalidNameException.php
+++ src/Backend/Modules/Locale/Domain/Locale/Name/InvalidNameException.php
@@ -6,12 +6,12 @@ use InvalidArgumentException;
 
 final class InvalidNameException extends InvalidArgumentException
 {
-    public static function onlyAlphanumeric()
+    public static function onlyAlphanumeric(): self
     {
         return new self('The name may only contain alphanumeric characters');
     }
 
-    public static function mustBeCapitalised()
+    public static function mustBeCapitalised(): self
     {
         return new self('The first character must be a capital letter [A-Z]');
     }
