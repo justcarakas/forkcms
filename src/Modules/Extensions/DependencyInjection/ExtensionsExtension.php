18642c5f54ff9ab6379e606513ce008e98361196
Jelmer Prins <jelmer@pageon.be>
2021-05-24 23:17:50 +0200

    Add dependency injection to the extensions module

---
+++ src/Modules/Extensions/DependencyInjection/ExtensionsExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ForkCMS\Core\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class ExtensionsExtension extends Extension implements PrependExtensionInterface
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $this->getLoader($container)->load('services.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $this->getLoader($container)->load('doctrine.yaml');
+    }
+
+    private function getLoader(ContainerBuilder $container): YamlFileLoader
+    {
+        return  new YamlFileLoader($container, new FileLocator(__DIR__ . '/../config'));
+    }
+}
