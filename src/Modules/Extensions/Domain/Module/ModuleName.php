0b77cc53c1eafbe403ad151e2ad01ebfd0628954
Jelmer Prins <jelmer@pageon.be>
2021-05-24 23:18:12 +0200

    Create value object for module name

---
+++ src/Modules/Extensions/Domain/Module/ModuleName.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ForkCMS\Modules\Extensions\Domain\Module;
+
+use Webmozart\Assert\Assert;
+
+final class ModuleName
+{
+    public const NAME_VALIDATION_REGEX = '^[A-Z][A-Za-z0-9]*$';
+
+    private string $name;
+
+    private function __construct(string $name)
+    {
+        Assert::regex($name, self::NAME_VALIDATION_REGEX, 'Invalid module name');
+
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public static function fromString(string $name): self
+    {
+        return new self($name);
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+}
