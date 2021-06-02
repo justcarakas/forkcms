5484d24c3454d8b7d135885d8c64bb5e8a3a5a6a
Jelmer Prins <jelmer@pageon.be>
2021-06-02 08:46:37 +0200

    use the Installer step in the abstract controller and requirements check

--- src/Core/Installer/Controller/RequirementCheckerController.php
+++ src/Core/Installer/Controller/RequirementCheckerController.php
@@ -2,11 +2,13 @@
 
 namespace ForkCMS\Core\Installer\Controller;
 
+use ForkCMS\Core\Installer\Domain\Installer\InstallerStep;
 use ForkCMS\Core\Installer\Domain\Requirement\RequirementsChecker;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\RouterInterface;
 use Twig\Environment;
 
@@ -19,23 +21,26 @@ final class RequirementCheckerController extends AbstractStepController
         RouterInterface $router,
         RequirementsChecker $requirementsChecker,
         FormFactoryInterface $formFactory,
+        MessageBusInterface $commandBus,
         string $rootDir
     ) {
-        parent::__construct($twig, $router, $requirementsChecker, $formFactory);
+        parent::__construct($twig, $router, $requirementsChecker, $formFactory, $commandBus);
 
         $this->rootDir = $rootDir;
     }
 
     public function __invoke(Request $request): Response
     {
+        $step = InstallerStep::requirements();
+
         // if all our requirements are met, go to the next step
         if ($this->requirementsChecker->passes()) {
-            return new RedirectResponse($this->router->generate('install_step2'));
+            return new RedirectResponse($this->router->generate($step->next()->route()));
         }
 
         return new Response(
             $this->twig->render(
-                'step1.html.twig',
+                $step->template(),
                 [
                     'checker' => $this->requirementsChecker,
                     'rootDir' => realpath($this->rootDir),
