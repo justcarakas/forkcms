0d2fabe5f45a0b7616865627e667d07548fe5677
Jelmer Prins <jelmer@pageon.be>
2021-06-03 13:18:02 +0200

    Add a locale form type

---
+++ src/Core/Domain/Locale/LocaleType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ForkCMS\Core\Domain\Locale;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LocaleType extends AbstractType implements DataTransformerInterface
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->addModelTransformer($this);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults(
+            [
+                'choices' => array_flip(Locale::toArray()),
+            ]
+        );
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'fork_locale';
+    }
+
+    public function getParent(): string
+    {
+        return ChoiceType::class;
+    }
+
+    public function transform($value): array|Locale|null
+    {
+        return $value;
+    }
+
+    public function reverseTransform($value): array|Locale|null
+    {
+        return match (true) {
+            empty($value) => $value,
+            is_array($value) => array_map(
+                static function (string $localeCode) {
+                    return Locale::from($localeCode);
+                },
+                $value
+            ),
+            default => Locale::from($value),
+        };
+    }
+}
