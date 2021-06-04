3ca7e3d9fff74051c3ca790142b58ec21d28c32e
Jelmer Prins <jelmer@pageon.be>
2021-06-05 01:15:34 +0200

    Implement the installer install step

---
+++ src/Core/Installer/Controller/InstallController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ForkCMS\Core\Installer\Controller;
+
+use ForkCMS\Core\Installer\Domain\Installer\InstallerConfiguration;
+use ForkCMS\Core\Installer\Domain\Installer\InstallerStep;
+use ForkCMS\Core\Installer\Domain\Installer\InstallForkCMS;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Twig\Environment;
+
+final class InstallController
+{
+    public function __construct(
+        private Environment $twig,
+        private RouterInterface $router,
+        private MessageBusInterface $commandBus
+    ) {
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        $step = InstallerStep::install();
+        $installerConfiguration = InstallerConfiguration::fromSession($request->getSession());
+
+        if (!$installerConfiguration->isValidForStep($step)) {
+            return new RedirectResponse($this->router->generate($step->previous()->route()));
+        }
+
+        $this->commandBus->dispatch(new InstallForkCMS($installerConfiguration));
+
+        return new Response(
+            $this->twig->render(
+                $step->template(),
+                [
+                    'data' => $installerConfiguration,
+                ]
+            )
+        );
+    }
+}
