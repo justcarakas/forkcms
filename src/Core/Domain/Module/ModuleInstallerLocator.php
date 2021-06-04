92bbc052d9139e404570d3c7ea6451a7f592f1b0
Jelmer Prins <jelmer@pageon.be>
2021-06-04 14:36:28 +0200

    Add base class for module installer and a locator for it

---
+++ src/Core/Domain/Module/ModuleInstallerLocator.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace ForkCMS\Core\Domain\Module;
+
+use InvalidArgumentException;
+
+final class ModuleInstallerLocator
+{
+    /** @var array<string, ModuleInstaller> */
+    private array $moduleInstallers;
+
+    /** @param iterable|ModuleInstaller[] $moduleInstallers */
+    public function __construct(iterable $moduleInstallers)
+    {
+        $this->moduleInstallers = [];
+        foreach ($moduleInstallers as $moduleInstaller) {
+            $this->moduleInstallers[$moduleInstaller->getModuleName()->getName()] = $moduleInstaller;
+        }
+    }
+
+    public function getModuleInstaller(ModuleName $moduleName): ModuleInstaller
+    {
+        return $this->moduleInstallers[$moduleName->getName()]
+               ?? throw new InvalidArgumentException('No installer was found for the module: ' . $moduleName);
+    }
+
+    /** @return ModuleName[] */
+    public function getAllModuleNames(): array
+    {
+        return $this->moduleInstallersToModuleNames($this->moduleInstallers);
+    }
+
+    /** @return ModuleName[] */
+    public function getRequiredModuleNames(): array
+    {
+        return $this->moduleInstallersToModuleNames(
+            array_filter(
+                $this->moduleInstallers,
+                static fn(ModuleInstaller $moduleInstaller) => $moduleInstaller->isRequired()
+            )
+        );
+    }
+
+    /** @return ModuleName[] */
+    public function getModuleNamesForOverview(): array
+    {
+        return $this->moduleInstallersToModuleNames(
+            array_filter(
+                $this->moduleInstallers,
+                static fn(ModuleInstaller $moduleInstaller) => $moduleInstaller->isVisibleInOverview()
+            )
+        );
+    }
+
+    /**
+     * @param ModuleInstaller[] $moduleInstallers
+     * @return ModuleName[]
+     */
+    private function moduleInstallersToModuleNames(array $moduleInstallers): array
+    {
+        return array_map(
+            static fn(ModuleInstaller $moduleInstaller): ModuleName => $moduleInstaller->getModuleName(),
+            $moduleInstallers
+        );
+    }
+}
