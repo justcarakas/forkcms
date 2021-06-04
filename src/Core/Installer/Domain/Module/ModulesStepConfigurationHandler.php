9fa69eca3b554ef538a602860778c6e2c72f2641
Jelmer Prins <jelmer@pageon.be>
2021-06-04 19:01:38 +0200

    Implement the installer modules step

---
+++ src/Core/Installer/Domain/Module/ModulesStepConfigurationHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace ForkCMS\Core\Installer\Domain\Module;
+
+use ForkCMS\Core\Domain\MessageHandler\CommandHandlerInterface;
+use ForkCMS\Core\Domain\Module\ModuleInstallerLocator;
+use ForkCMS\Core\Installer\Domain\Installer\InstallerConfiguration;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+final class ModulesStepConfigurationHandler implements CommandHandlerInterface
+{
+    public function __construct(
+        private SessionInterface $session,
+        private ModuleInstallerLocator $moduleInstallerLocator
+    ) {
+    }
+
+    public function __invoke(ModulesStepConfiguration $modulesStepConfiguration)
+    {
+        InstallerConfiguration::fromSession($this->session)->withModulesStep(
+            $modulesStepConfiguration,
+            $this->moduleInstallerLocator
+        );
+    }
+}
