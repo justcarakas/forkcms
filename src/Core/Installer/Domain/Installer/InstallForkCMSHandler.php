7103d89761f384eee1f1ca0b83ff0c476b6ec567
Jelmer Prins <jelmer@pageon.be>
2021-06-05 15:05:22 +0200

    Write installation configuration to yaml when requested

--- src/Core/Installer/Domain/Installer/InstallForkCMSHandler.php
+++ src/Core/Installer/Domain/Installer/InstallForkCMSHandler.php
@@ -3,10 +3,19 @@
 namespace ForkCMS\Core\Installer\Domain\Installer;
 
 use ForkCMS\Core\Domain\MessageHandler\CommandHandlerInterface;
+use ForkCMS\Core\Installer\Domain\Configuration\ConfigurationParser;
 
 final class InstallForkCMSHandler implements CommandHandlerInterface
 {
+    public function __construct(private ConfigurationParser $configurationParser)
+    {
+    }
+
     public function __invoke(InstallForkCMS $installForkCMS)
     {
+        $installerConfiguration = $installForkCMS->getInstallerConfiguration();
+        if ($installerConfiguration->shouldSaveConfiguration()) {
+            $this->configurationParser->toFile($installForkCMS->getInstallerConfiguration());
+        }
     }
 }
