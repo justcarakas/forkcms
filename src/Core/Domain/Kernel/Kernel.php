93ba1003d49b472b17d052f90db6bbb8f3d1f3af
Jelmer Prins <jelmer@pageon.be>
2021-06-08 23:37:32 +0200

    Move the kernel to its own namespace

--- src/Core/Domain/Kernel/Kernel.php
+++ src/Core/Domain/Kernel/Kernel.php
@@ -1,21 +1,25 @@
 <?php
 
-namespace ForkCMS;
+namespace ForkCMS\Core\Domain\Kernel;
 
+use ForkCMS\Core\DependencyInjection\CoreExtension;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
+    private const ROOT_DIR = __DIR__ . '/../../../../';
+
     use MicroKernelTrait;
 
     private bool $isInstalled;
 
     public function __construct(string $environment, bool $debug)
     {
-        $this->isInstalled = file_exists(__DIR__ . '/../.env.local');
+        $this->isInstalled = file_exists(self::ROOT_DIR . '.env.local');
 
         if (!$this->isInstalled) {
             $environment = str_contains($environment, 'test') ? 'test_install' : 'install';
@@ -26,7 +30,7 @@ class Kernel extends BaseKernel
 
     private function isInstalled(): bool
     {
-        return $this->isInstalled || str_ends_with($this->environment, 'installer');
+        return $this->isInstalled || !str_ends_with($this->environment, 'installer');
     }
 
     protected function configureContainer(ContainerConfigurator $container): void
@@ -38,6 +42,15 @@ class Kernel extends BaseKernel
         }
 
         $this->configureInstallerContainer($container);
+    }
+
+    protected function buildContainer(): ContainerBuilder
+    {
+        $container = parent::buildContainer();
+
+        $container->registerExtension(new CoreExtension());
+
+        return $container;
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void
@@ -53,40 +66,38 @@ class Kernel extends BaseKernel
 
     private function configureLiveContainer(ContainerConfigurator $container): void
     {
-        $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
+        $container->import(self::ROOT_DIR . 'config/{packages}/*.yaml');
+        $container->import(self::ROOT_DIR . 'config/{packages}/' . $this->environment . '/*.yaml');
 
-        $container->import('../config/services.yaml');
-        $container->import('../config/{services}_' . $this->environment . '.yaml');
+        $container->import(self::ROOT_DIR . 'config/services.yaml');
+        $container->import(self::ROOT_DIR . 'config/{services}_' . $this->environment . '.yaml');
     }
 
     private function configureInstallerContainer(ContainerConfigurator $container): void
     {
-        $container->import('../config/{packages}/install/*.yaml');
+        $container->import(self::ROOT_DIR . 'config/{packages}/install/*.yaml');
         if ($this->environment === 'test_install') {
-            $container->import('../config/{packages}/test_install/*.yaml');
+            $container->import(self::ROOT_DIR . 'config/{packages}/test_install/*.yaml');
         }
 
-        $container->import('../config/{services}_install.yaml');
+        $container->import(self::ROOT_DIR . 'config/{services}_install.yaml');
         if ($this->environment === 'test_install') {
-            $container->import('../config/{services}_test_install.yaml');
+            $container->import(self::ROOT_DIR . 'config/{services}_test_install.yaml');
         }
     }
 
     private function configureLiveRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/' . $this->environment . '/*.yaml');
-        $routes->import('../config/{routes}/*.yaml');
+        $routes->import(self::ROOT_DIR . 'config/{routes}/' . $this->environment . '/*.yaml');
+        $routes->import(self::ROOT_DIR . 'config/{routes}/*.yaml');
 
-        if (is_file(\dirname(__DIR__) . '/config/routes.yaml')) {
-            $routes->import('../config/routes.yaml');
-        } elseif (is_file($path = \dirname(__DIR__) . '/config/routes.php')) {
-            (require $path)($routes->withPath($path), $this);
+        if (is_file(self::ROOT_DIR . 'config/routes.yaml')) {
+            $routes->import(self::ROOT_DIR . 'config/routes.yaml');
         }
     }
 
     private function configureInstallerRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/install/*.yaml');
+        $routes->import(self::ROOT_DIR . 'config/{routes}/install/*.yaml');
     }
 }
