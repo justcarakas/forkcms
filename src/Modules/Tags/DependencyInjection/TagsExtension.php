5e137ad4f1742a701f92be3f6c2081a92174624a
Jelmer Prins <jelmer@pageon.be>
2021-06-11 12:52:25 +0200

    Add some fork magic to the dependency injection in Fork

--- src/Modules/Tags/DependencyInjection/TagsExtension.php
+++ src/Modules/Tags/DependencyInjection/TagsExtension.php
@@ -2,13 +2,15 @@
 
 namespace ForkCMS\Modules\Tags\DependencyInjection;
 
+use ForkCMS\Core\Domain\DependencyInjection\ForkModuleExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-final class TagsExtension extends Extension implements PrependExtensionInterface
+/**
+ * This is the class that loads and manages your module configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+final class TagsExtension extends ForkModuleExtension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
@@ -18,13 +20,5 @@ final class TagsExtension extends Extension implements PrependExtensionInterface
     public function prepend(ContainerBuilder $container): void
     {
         $this->getLoader($container)->load('doctrine.yaml');
-    }
-
-    public function getLoader(ContainerBuilder $container): YamlFileLoader
-    {
-        return new YamlFileLoader(
-            $container,
-            new FileLocator(__DIR__ . '/../Resources/config')
-        );
     }
 }
