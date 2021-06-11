a6ab8432ab14fc9792d1dd757cc0597398f62462
Jelmer Prins <jelmer@pageon.be>
2021-06-11 12:55:22 +0200

    Add command to install modules

--- src/Modules/Extensions/Domain/Module/InstallModules.php
+++ src/Modules/Extensions/Domain/Module/InstallModules.php
@@ -4,5 +4,15 @@ namespace ForkCMS\Modules\Extensions\Domain\Module;
 
 final class InstallModules
 {
+    private array $moduleNames;
 
+    public function __construct(ModuleName ...$moduleNames)
+    {
+        $this->moduleNames = $moduleNames;
+    }
+
+    public function getModuleNames(): array
+    {
+        return $this->moduleNames;
+    }
 }
