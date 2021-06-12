29a9e33ddf6e9f94c88dc65b74461ea7b2d3daa0
Jelmer Prins <jelmer@pageon.be>
2021-06-12 14:50:31 +0200

    Make it easier to configure if a module is required or visible without overwriting the contructor

--- src/Modules/Backend/Installer/BackendInstaller.php
+++ src/Modules/Backend/Installer/BackendInstaller.php
@@ -1,17 +1,19 @@
 <?php
 
-namespace ForkCMS\Modules\Groups\Installer;
+namespace ForkCMS\Modules\Backend\Installer;
+
 
 use ForkCMS\Modules\Extensions\Domain\Module\ModuleInstaller;
 use ForkCMS\Modules\Extensions\Domain\Module\ModuleName;
 
-final class GroupsInstaller extends ModuleInstaller
+final class BackendInstaller extends ModuleInstaller
 {
     public const IS_REQUIRED = true;
+    public const IS_VISIBLE_IN_OVERVIEW = false;
 
     public static function getModuleName(): ModuleName
     {
-        return ModuleName::fromString('Groups');
+        return ModuleName::fromString('Backend');
     }
 
     public function install(): void
