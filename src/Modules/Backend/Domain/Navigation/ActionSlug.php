c1df8a232fc965085a393223f7fd9012ae6da5d9
Jelmer Prins <jelmer@pageon.be>
2021-06-14 21:48:18 +0200

    Create way to get a backend action from a slug

---
+++ src/Modules/Backend/Domain/Navigation/ActionSlug.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace ForkCMS\Modules\Backend\Domain\Navigation;
+
+use Assert\Assert;
+use ForkCMS\Modules\Extensions\Domain\Action\ActionName;
+use ForkCMS\Modules\Extensions\Domain\Module\ModuleName;
+use InvalidArgumentException;
+use Symfony\Component\DependencyInjection\Container;
+
+final class ActionSlug
+{
+    private function __construct(private ModuleName $moduleName, private ActionName $actionName)
+    {
+        Assert::that($this->getFQCN())->classExists('Action class not found');
+    }
+
+    public static function fromSlug(string $slug): self
+    {
+        $matches = [];
+        if (!preg_match(
+            '#(^[a-z][a-z0-9_]*[a-z0-9]*)/([a-z][a-z0-9_]*[a-z0-9]*$)#',
+            $slug,
+            $matches
+        )) {
+            throw new InvalidArgumentException('Slug could not be matched to a module and an action');
+        }
+
+        return new self(
+            ModuleName::fromString(Container::camelize($matches[1])),
+            ActionName::fromString(Container::camelize($matches[2]))
+        );
+    }
+
+    public static function fromFQCN(string $fullyQualifiedClassName): self
+    {
+        $matches = [];
+        if (!preg_match(
+            '/^ForkCMS\\\Modules\\\([A-Z]\w*)\\\Backend\\\Actions\\\([A-Z]\w*$)/',
+            $fullyQualifiedClassName,
+            $matches
+        )) {
+            throw new InvalidArgumentException('Can ony be created from a backen action class name');
+        }
+
+        return new self(ModuleName::fromString($matches[1]), ActionName::fromString($matches[2]));
+    }
+
+    public function getFQCN(): string
+    {
+        return 'ForkCMS\\Modules\\' . $this->moduleName . '\\Backend\\Actions\\' . $this->actionName;
+    }
+
+    public function getSlug(): string
+    {
+        return implode(
+            '/',
+            [
+                Container::underscore($this->moduleName->getName()),
+                Container::underscore($this->actionName->getName()),
+            ]
+        );
+    }
+
+    public function __toString(): string
+    {
+        return $this->getSlug();
+    }
+}
