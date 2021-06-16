2725b85ab74180da09f4112c64a0796de71fd0cd
Jelmer Prins <jelmer@pageon.be>
2021-06-16 13:27:13 +0200

    Add method to add navigation items in the installer

--- src/Modules/Extensions/Domain/Module/ModuleInstaller.php
+++ src/Modules/Extensions/Domain/Module/ModuleInstaller.php
@@ -3,9 +3,13 @@
 namespace ForkCMS\Modules\Extensions\Domain\Module;
 
 use ForkCMS\Core\Domain\Doctrine\CreateSchema;
+use ForkCMS\Modules\Backend\Domain\NavigationItem\ActionSlug;
+use ForkCMS\Modules\Backend\Domain\NavigationItem\NavigationItem;
+use ForkCMS\Modules\Backend\Domain\NavigationItem\NavigationItemRepository;
 use ForkCMS\Modules\Backend\Installer\BackendInstaller;
 use ForkCMS\Modules\Error\Installer\ErrorInstaller;
 use ForkCMS\Modules\Extensions\Installer\ExtensionsInstaller;
+use ForkCMS\Modules\Internationalisation\Domain\Translation\TranslationKey;
 use ForkCMS\Modules\Internationalisation\Installer\InternationalisationInstaller;
 
 abstract class ModuleInstaller
@@ -15,7 +19,6 @@ abstract class ModuleInstaller
     /** If the module should show up on a list of installed or installable modules */
     public const IS_VISIBLE_IN_OVERVIEW = true;
 
-    protected bool $isVisibleInOverview = true;
     /** @var array<string,ModuleName> */
     private array $moduleDependencies = [];
     private ?array $defaultModuleDependencies = null;
@@ -23,6 +26,7 @@ abstract class ModuleInstaller
     public function __construct(
         private CreateSchema $createSchema,
         private ModuleRepository $moduleRepository,
+        private NavigationItemRepository $navigationRepository,
     ) {
     }
 
@@ -94,5 +98,57 @@ abstract class ModuleInstaller
         $this->defaultModuleDependencies[$extensionInstaller->getName()] = $extensionInstaller;
 
         return $this->defaultModuleDependencies;
+    }
+
+    protected function getOrCreateBackendNavigationItem(
+        TranslationKey $label,
+        ?ActionSlug $slug = null,
+        ?NavigationItem $parent = null,
+        ?int $sequence = null,
+    ): NavigationItem {
+        $navigationItem = $this->navigationRepository->findOneBy(
+            [
+                'label' => $label,
+                'slug' => $slug,
+                'parent' => $parent,
+            ]
+        );
+        if ($navigationItem instanceof NavigationItem) {
+            return $navigationItem;
+        }
+
+        $navigationItem = new NavigationItem($label, $slug, $parent, $sequence);
+        $this->navigationRepository->save($navigationItem);
+
+        return $navigationItem;
+    }
+
+    protected function getModulesNavigationItem(): NavigationItem
+    {
+        return $this->getOrCreateBackendNavigationItem(
+            TranslationKey::label('Modules'),
+            null,
+            null,
+            4,
+        );
+    }
+
+    protected function getSettingsNavigationItem(): NavigationItem
+    {
+        return $this->getOrCreateBackendNavigationItem(
+            TranslationKey::label('Settings'),
+            null,
+            null,
+            999,
+        );
+    }
+
+    protected function getModuleSettingsNavigationItem(): NavigationItem
+    {
+        return $this->getOrCreateBackendNavigationItem(
+            TranslationKey::label('Modules'),
+            null,
+            $this->getSettingsNavigationItem(),
+        );
     }
 }
