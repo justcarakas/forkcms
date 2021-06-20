f309c31d2700ebe63797f8c429bc00937afc6619
Jelmer Prins <jelmer@pageon.be>
2021-06-20 17:59:53 +0200

    Add user group permissions

---
+++ src/Modules/Backend/Domain/UserGroup/UserGroupModule.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace ForkCMS\Modules\Backend\Domain\UserGroup;
+
+use Doctrine\ORM\Mapping as ORM;
+use ForkCMS\Modules\Extensions\Domain\Module\ModuleName;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="user_group_modules")
+ */
+class UserGroupModule
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @Orm\ManyToOne(targetEntity="UserGroup", inversedBy="modules")
+     * @Orm\JoinColumn(referencedColumnName="id")
+     */
+    private UserGroup $userGroup;
+
+    /**
+     * @ORM\Column(type="modules__extensions__module__module_name")
+     */
+    private ModuleName $moduleName;
+
+    public function __construct(int $id, UserGroup $userGroup, ModuleName $moduleName)
+    {
+        $this->id = $id;
+        $this->userGroup = $userGroup;
+        $this->moduleName = $moduleName;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUserGroup(): UserGroup
+    {
+        return $this->userGroup;
+    }
+
+    public function getModuleName(): ModuleName
+    {
+        return $this->moduleName;
+    }
+}
