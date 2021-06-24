8636d09fa33610afe1b246a41f7cb9851338a7a6
Jelmer Prins <jelmer@pageon.be>
2021-06-25 00:40:23 +0200

    Make it possible to render the backend error page

--- src/Modules/Backend/Domain/Action/AbstractActionController.php
+++ src/Modules/Backend/Domain/Action/AbstractActionController.php
@@ -2,23 +2,29 @@
 
 namespace ForkCMS\Modules\Backend\Domain\Action;
 
+use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
 
 abstract class AbstractActionController implements ActionControllerInterface
 {
     private static ?ActionSlug $actionSlug = null;
     private string $templatePath;
+    private string $pageTitle;
+    private array $twigContext = [];
 
-    public function __construct(private Environment $twig)
+    public function __construct(private Environment $twig, private TranslatorInterface $translator)
     {
         $actionSlug = self::getActionSlug();
         $this->templatePath = sprintf(
-            '@%s/%s.html.twig',
+            '@%s/Backend/Actions/%s.html.twig',
             $actionSlug->getModuleName(),
             $actionSlug->getActionName()
         );
+
+        $this->pageTitle = $actionSlug->getModuleName()->asLabel();
     }
 
     final public function changeTemplatePath(string $templatePath): void
@@ -33,6 +39,29 @@ abstract class AbstractActionController implements ActionControllerInterface
 
     public function __invoke(Request $request): Response
     {
-        return new Response($this->twig->render($this->templatePath));
+        $actionSlug = self::getActionSlug();
+        $this->twig->addGlobal('INTERFACE_LANGUAGE', $request->getLocale());
+        $this->twig->addGlobal('SITE_TITLE', $_ENV['SITE_DEFAULT_TITLE']);
+        $this->twig->addGlobal('page_title', $this->translator->trans($this->pageTitle));
+        $this->twig->addGlobal('jsFiles', []);
+        $this->twig->addGlobal('jsData', null);
+        $this->twig->addGlobal('bodyID', Container::underscore($actionSlug->getModuleName()));
+        $this->twig->addGlobal('bodyClass', str_replace('/', '_', $actionSlug->getSlug()));
+
+        $this->execute($request);
+
+        return $this->getResponse();
+    }
+
+    public function assign(string $key, mixed $value): void
+    {
+        $this->twigContext[$key] = $value;
+    }
+
+    abstract protected function execute(Request $request): void;
+
+    public function getResponse(): Response
+    {
+        return new Response($this->twig->render($this->templatePath, $this->twigContext));
     }
 }
