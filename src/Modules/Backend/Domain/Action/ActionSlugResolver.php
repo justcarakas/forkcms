def551935bca64ac297c6db2369d281be574ddd3
Jelmer Prins <jelmer@pageon.be>
2021-06-24 20:33:35 +0200

    Add a resolver for action slugs

---
+++ src/Modules/Backend/Domain/Action/ActionSlugResolver.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ForkCMS\Modules\Backend\Domain\Action;
+
+use Generator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
+
+final class ActionSlugResolver implements ArgumentValueResolverInterface
+{
+    public function supports(Request $request, ArgumentMetadata $argument): bool
+    {
+        return $argument->getType() === ActionSlug::class;
+    }
+
+    public function resolve(Request $request, ArgumentMetadata $argument): Generator
+    {
+        yield ActionSlug::fromRequest($request);
+    }
+}
