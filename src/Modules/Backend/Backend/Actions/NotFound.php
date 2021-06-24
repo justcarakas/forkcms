0675bd967a01cadcd64f4127ce9d709438ba1509
Jelmer Prins <jelmer@pageon.be>
2021-06-25 00:40:23 +0200

    Make it possible to render the backend error page

--- src/Modules/Backend/Backend/Actions/NotFound.php
+++ src/Modules/Backend/Backend/Actions/NotFound.php
@@ -3,7 +3,22 @@
 namespace ForkCMS\Modules\Backend\Backend\Actions;
 
 use ForkCMS\Modules\Backend\Domain\Action\AbstractActionController;
+use ForkCMS\Modules\Internationalisation\Domain\Translation\TranslationKey;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 final class NotFound extends AbstractActionController
 {
+    protected function execute(Request $request): void
+    {
+        $this->assign('message', TranslationKey::error('NotFound'));
+    }
+
+    public function getResponse(): Response
+    {
+        $response = parent::getResponse();
+        $response->setStatusCode(Response::HTTP_NOT_FOUND);
+
+        return $response;
+    }
 }
