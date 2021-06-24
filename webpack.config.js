6b5131dbde16390b76be7309b8b4cbba65aa2078
Jelmer Prins <jelmer@pageon.be>
2021-06-25 01:56:02 +0200

    Configure webpack for the backend

--- webpack.config.js
+++ webpack.config.js
@@ -82,74 +82,74 @@ Encore.reset()
 //
 // END INSTALLER SETUP
 //
-// // ===========================
-// //
-// // START BACKEND
-// //
+// ===========================
 //
-// // Manually configure the runtime environment if not already configured yet by the "encore" command.
-// // It's useful when you use tools that rely on webpack.config.js file.
-// if (!Encore.isRuntimeEnvironmentConfigured()) {
-//   // Set the runtime environment
-//   Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
-// }
+// START BACKEND
 //
-// Encore
-//   .setOutputPath('public/build/backend')
-//   .setPublicPath('/build/backend')
-//   .setManifestKeyPrefix('public/build/backend')
-//
-//   .addEntry('backend', './src/Core/Backend/Core/Js/Backend.js')
-//   .addStyleEntry('screen', './src/Core/Backend/Core/Layout/Sass/screen.scss')
-//
-//   .cleanupOutputBeforeBuild()
-//
-//   // will require an extra script tag for runtime.js
-//   // but, you probably want this, unless you're building a single-page app
-//   .enableSingleRuntimeChunk()
-//   .enableSassLoader((options) => {}, {
-//     resolveUrlLoader: false
-//   })
-//   .enablePostCssLoader()
-//   // enables @babel/preset-env polyfills
-//   .enableSourceMaps(!Encore.isProduction())
-//   // enables hashed filenames (e.g. app.abc123.css)
-//   .enableVersioning(Encore.isProduction())
-//
-//   .copyFiles({
-//     from: './src/Core/Backend/Core/Layout/images',
-//     to: './images/[path][name].[ext]'
-//   })
-//
-//   .autoProvidejQuery()
-//   .autoProvideVariables({
-//     moment: 'moment'
-//   })
-//
-//   // enables @babel/preset-env polyfills
-//   .configureBabel(() => {}, {
-//     useBuiltIns: 'usage',
-//     corejs: 3
-//   })
-//
-//   .configureWatchOptions((watchOptions) => {
-//     watchOptions.poll = 250
-//   })
-//
-//   .enableBuildNotifications(true, (options) => {
-//     options.alwaysNotify = true
-//   })
-//
-//   .addPlugin(new LiveReloadPlugin())
-//
-// // build the first configuration
-// const backendConfig = Encore.getWebpackConfig()
-//
-// // Set a unique name for the config (needed later!)
-// backendConfig.name = 'backendConfig'
-//
-// // reset Encore to build the next config
-// Encore.reset()
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  // Set the runtime environment
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
+}
+
+Encore
+  .setOutputPath('public/build/backend')
+  .setPublicPath('/build/backend')
+  .setManifestKeyPrefix('public/build/backend')
+
+  .addEntry('backend', './src/Core/assets/js/Backend/Backend.js')
+  .addStyleEntry('screen', './src/Core/assets/sass/Backend/screen.scss')
+
+  .cleanupOutputBeforeBuild()
+
+  // will require an extra script tag for runtime.js
+  // but, you probably want this, unless you're building a single-page app
+  .enableSingleRuntimeChunk()
+  .enableSassLoader((options) => {}, {
+    resolveUrlLoader: true
+  })
+  .enablePostCssLoader()
+  // enables @babel/preset-env polyfills
+  .enableSourceMaps(!Encore.isProduction())
+  // enables hashed filenames (e.g. app.abc123.css)
+  .enableVersioning(Encore.isProduction())
+
+  .copyFiles({
+    from: './src/Core/assets/images/Backend',
+    to: './images/[path][name].[ext]'
+  })
+
+  .autoProvidejQuery()
+  .autoProvideVariables({
+    moment: 'moment'
+  })
+
+  // enables @babel/preset-env polyfills
+  .configureBabel(() => {}, {
+    useBuiltIns: 'usage',
+    corejs: 3
+  })
+
+  .configureWatchOptions((watchOptions) => {
+    watchOptions.poll = 250
+  })
+
+  .enableBuildNotifications(true, (options) => {
+    options.alwaysNotify = true
+  })
+
+  .addPlugin(new LiveReloadPlugin())
+
+// build the first configuration
+const backendConfig = Encore.getWebpackConfig()
+
+// Set a unique name for the config (needed later!)
+backendConfig.name = 'backendConfig'
+
+// reset Encore to build the next config
+Encore.reset()
 
 //
 // END BACKEND SETUP
@@ -304,4 +304,4 @@ Encore.reset()
 // // END FRONTEND THEME SETUP
 //
 // module.exports = [installerConfig, backendConfig, frontendConfig, frontendThemeConfig]
-module.exports = [installerConfig]
+module.exports = [installerConfig, backendConfig]
