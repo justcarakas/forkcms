2b59f6f7180334d44c11dc5c5c638832b9a21a59
Jelmer Prins <jelmer@pageon.be>
2021-06-24 22:01:28 +0200

    Add the backend controller

--- src/Modules/Backend/Controller/BackendController.php
+++ src/Modules/Backend/Controller/BackendController.php
@@ -2,12 +2,37 @@
 
 namespace ForkCMS\Modules\Backend\Controller;
 
+use ForkCMS\Modules\Backend\Domain\Action\ActionControllerInterface;
+use ForkCMS\Modules\Backend\Domain\Action\ActionSlug;
+use InvalidArgumentException;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Component\DependencyInjection\ServiceLocator;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final class BackendController
 {
-    public function __invoke(): Response
+    public function __construct(private ServiceLocator $actions)
     {
-        return new Response();
+    }
+
+    public function __invoke(
+        Request $request,
+        ?string $locale,
+        ActionSlug $actionSlug
+    ): Response {
+        try {
+            $action = $this->actions->get($actionSlug->getFQCN());
+        } catch (NotFoundExceptionInterface) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'The action class %s must be registered as a service and implement %s',
+                    $actionSlug->getFQCN(),
+                    ActionControllerInterface::class
+                )
+            );
+        }
+
+        return $action($request);
     }
 }
