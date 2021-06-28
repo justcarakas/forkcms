dc3e6f5bd544d7912a94061bc3ff05f2fd8e1e19
Jelmer Prins <jelmer@pageon.be>
2021-06-28 15:08:00 +0200

    Don't load the backend controllers while fork isn't installed

--- src/Modules/Backend/DependencyInjection/BackendExtension.php
+++ src/Modules/Backend/DependencyInjection/BackendExtension.php
@@ -10,6 +10,9 @@ class BackendExtension extends ForkModuleExtension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $this->getLoader($container)->load('services.yaml');
+        if ($container->getParameter('fork.is_installed')) {
+            $this->getLoader($container)->load('controllers.yaml');
+        }
     }
 
     public function prepend(ContainerBuilder $container): void
