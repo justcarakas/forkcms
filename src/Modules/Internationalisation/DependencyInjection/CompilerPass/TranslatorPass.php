4d4160b8e9350c80053cad108bdcc3753464092e
Jelmer Prins <jelmer@pageon.be>
2021-06-30 01:56:56 +0200

    Set the enabled locales based on the database settings

--- src/Modules/Internationalisation/DependencyInjection/CompilerPass/TranslatorPass.php
+++ src/Modules/Internationalisation/DependencyInjection/CompilerPass/TranslatorPass.php
@@ -3,10 +3,12 @@
 namespace ForkCMS\Modules\Internationalisation\DependencyInjection\CompilerPass;
 
 use ForkCMS\Core\Domain\Application\Application;
+use ForkCMS\Core\Domain\PDO\ForkConnection;
 use ForkCMS\Modules\Extensions\Domain\Module\InstalledModules;
 use ForkCMS\Modules\Internationalisation\Domain\Locale\Locale;
 use ForkCMS\Modules\Internationalisation\Domain\Translation\TranslationDomain;
 use ForkCMS\Modules\Internationalisation\Domain\Translator\ForkTranslator;
+use PDO;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
@@ -17,30 +19,29 @@ final class TranslatorPass implements CompilerPassInterface
     public function process(ContainerBuilder $container): void
     {
         $domains = $this->getDatabaseDomains(InstalledModules::fromContainer($container));
-        $locales = $this->getDatabaseLocales();
+        $locales = $this->getDatabaseLocales($container);
+        $defaultLocale = array_search(true, $locales);
         $translator = $container->getDefinition('translator.default');
         $translator->setClass(ForkTranslator::class);
         $translator->addArgument(new Reference(RequestStack::class));
         $container->prependExtensionConfig(
             'framework',
             [
+                'default_locale' => $defaultLocale,
                 'translator' => [
-                    'enabled_locale' => array_map(
-                        static fn(Locale $locale): string => $locale->value,
-                        $locales
-                    ),
+                    'enabled_locale' => array_keys($locales),
                 ],
             ]
         );
 
         foreach ($domains as $domain) {
-            foreach ($locales as $locale) {
+            foreach ($locales as $locale => $isDefault) {
                 $translator->addMethodCall(
                     'addResource',
                     array(
                         'db',
                         null,
-                        $locale->value,
+                        $locale,
                         $domain->getDomain(),
                     )
                 );
@@ -61,9 +62,22 @@ final class TranslatorPass implements CompilerPassInterface
         return $translationDomains;
     }
 
-    /** @return Locale[] */
-    private function getDatabaseLocales(): array
+    /** @return array<string, bool> */
+    private function getDatabaseLocales(ContainerBuilder $container): array
     {
-        return Locale::cases();
+        if (!$container->getParameter('fork.is_installed')) {
+            $locales[Locale::en()->value] = true;
+
+            return $locales;
+        }
+
+        return array_map(
+            static fn(string $isEnabled): bool => $isEnabled,
+            ForkConnection::get()->query(
+                'SELECT locale, isDefaultForWebsite
+            FROM locales
+            WHERE isEnabledForWebsite = true OR isEnabledForUser = true'
+            )->fetchAll(PDO::FETCH_KEY_PAIR)
+        );
     }
 }
