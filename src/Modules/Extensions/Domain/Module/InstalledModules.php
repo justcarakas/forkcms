a3186baa0d261edb207f8e8fbbc9269ceb04b837
Jelmer Prins <jelmer@pageon.be>
2021-07-02 16:14:52 +0200

    Implement the new version of the forkcms:installer:install command

--- src/Modules/Extensions/Domain/Module/InstalledModules.php
+++ src/Modules/Extensions/Domain/Module/InstalledModules.php
@@ -4,13 +4,14 @@ namespace ForkCMS\Modules\Extensions\Domain\Module;
 
 use ForkCMS\Core\Domain\PDO\ForkConnection;
 use ForkCMS\Core\Installer\Domain\Configuration\InstallerConfiguration;
-use PDO;
-use RuntimeException;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Session\Session;
 
 final class InstalledModules
 {
+    /** @var ModuleName[] needed for the console install command  */
+    private static array $modulesToInstall = [];
+
     public function __construct(private bool $forkIsInstalled)
     {
     }
@@ -24,9 +25,18 @@ final class InstalledModules
     public function __invoke(): array
     {
         if (!$this->forkIsInstalled) {
+            if (count(self::$modulesToInstall) > 0) {
+                return self::$modulesToInstall;
+            }
+
             return InstallerConfiguration::fromSession(new Session())?->getModules() ?? [];
         }
 
         return ForkConnection::get()->getInstalledModules();
+    }
+
+    public static function setModulesToInstall(ModuleName ...$modulesToInstall): void
+    {
+        self::$modulesToInstall = $modulesToInstall;
     }
 }
