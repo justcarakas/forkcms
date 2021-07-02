90d79f2be18b580df00e501884454e7a52e6d5a3
Jelmer Prins <jelmer@pageon.be>
2021-07-02 18:32:16 +0200

    Register the actions for groups

---
+++ src/Modules/Backend/Backend/Actions/GroupEdit.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ForkCMS\Modules\Backend\Backend\Actions;
+
+use ForkCMS\Modules\Backend\Domain\Action\AbstractActionController;
+use Symfony\Component\HttpFoundation\Request;
+
+final class GroupEdit extends AbstractActionController
+{
+    protected function execute(Request $request): void
+    {
+        // TODO: Implement execute() method.
+        throw new \Exception('Method not implemented');
+    }
+}
