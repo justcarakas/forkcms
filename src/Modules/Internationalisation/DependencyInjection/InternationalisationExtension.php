ed37a803538b55371b47c795e15f6dcf89f343b0
Jelmer Prins <jelmer@pageon.be>
2021-07-03 12:28:21 +0200

    Create new endpoint for translations

--- src/Modules/Internationalisation/DependencyInjection/InternationalisationExtension.php
+++ src/Modules/Internationalisation/DependencyInjection/InternationalisationExtension.php
@@ -15,6 +15,10 @@ class InternationalisationExtension extends ForkModuleExtension
     public function load(array $configs, ContainerBuilder $container): void
     {
         $this->getLoader($container)->load('services.yaml');
+
+        if ($container->getParameter('fork.is_installed')) {
+            $this->getLoader($container)->load('controllers.yaml');
+        }
     }
 
     public function prepend(ContainerBuilder $container): void
