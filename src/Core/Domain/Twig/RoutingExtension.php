16faa542af0543c90bb689d59f00976f6f9d62e6
Jelmer Prins <jelmer@pageon.be>
2021-11-08 10:53:39 +0100

    Make action routes in twig

--- src/Core/Domain/Twig/RoutingExtension.php
+++ src/Core/Domain/Twig/RoutingExtension.php
@@ -2,21 +2,88 @@
 
 namespace ForkCMS\Core\Domain\Twig;
 
+use ForkCMS\Modules\Backend\Domain\Action\ActionName;
+use ForkCMS\Modules\Backend\Domain\Action\ActionSlug;
+use ForkCMS\Modules\Extensions\Domain\Module\ModuleName;
+use ForkCMS\Modules\Internationalisation\Domain\Locale\Locale;
+use Symfony\Bridge\Twig\Extension\RoutingExtension as TwigBridgeRoutingExtension;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Extension\AbstractExtension;
-use Twig\TwigFilter;
+use Twig\TwigFunction;
 
-final class PhpExtension extends AbstractExtension
+final class RoutingExtension extends AbstractExtension
 {
-    public function getFilters(): array
+    public function __construct(
+        private UrlGeneratorInterface $generator,
+        private TwigBridgeRoutingExtension $twigBridgeRoutingExcension,
+        private RequestStack $requestStack
+    ) {
+    }
+
+    public function getFunctions(): array
     {
         return [
-            new TwigFilter('urlencode', 'urlencode'),
-            new TwigFilter('rawurlencode', 'rawurlencode'),
-            new TwigFilter('striptags', 'strip_tags'),
-            new TwigFilter('addslashes', 'addslashes'),
-            new TwigFilter('count', 'count'),
-            new TwigFilter('is_array', 'is_array'),
-            new TwigFilter('ucfirst', 'ucfirst'),
+            new TwigFunction(
+                'action_url',
+                [$this, 'getUrl'],
+                ['is_safe_callback' => [$this->twigBridgeRoutingExcension, 'isUrlGenerationSafe']]
+            ),
+            new TwigFunction(
+                'action_path',
+                [$this, 'getPath'],
+                ['is_safe_callback' => [$this->twigBridgeRoutingExcension, 'isUrlGenerationSafe']]
+            ),
         ];
+    }
+
+    public function getPath(
+        string $actionName = null,
+        string $moduleName = null,
+        array $parameters = [],
+        bool $relative = false,
+        ?string $locale = null
+    ): string {
+        return $this->getActionSlug($moduleName, $actionName)->generateRoute(
+            $this->generator,
+            $parameters,
+            $relative ? UrlGeneratorInterface::RELATIVE_PATH : UrlGeneratorInterface::ABSOLUTE_PATH,
+            $locale === null ? null : Locale::tryFrom($locale)
+        );
+    }
+
+    public function getUrl(
+        string $actionName = null,
+        string $moduleName = null,
+        array $parameters = [],
+        bool $schemeRelative = false,
+        ?string $locale = null
+    ): string {
+        return $this->getActionSlug($moduleName, $actionName)->generateRoute(
+            $this->generator,
+            $parameters,
+            $schemeRelative ? UrlGeneratorInterface::NETWORK_PATH : UrlGeneratorInterface::ABSOLUTE_URL,
+            $locale === null ? null : Locale::tryFrom($locale)
+        );
+    }
+
+    private function getActionSlug(?string $moduleName, ?string $actionName): ActionSlug
+    {
+        $defaultSlug = ActionSlug::fromRequest($this->requestStack->getMainRequest());
+        if ($moduleName === null && $actionName === null) {
+            return $defaultSlug;
+        }
+
+        if ($moduleName === null) {
+            $moduleName = $defaultSlug->getModuleName()->getName();
+        }
+        if ($actionName === null) {
+            $actionName = $defaultSlug->getActionName()->getName();
+        }
+
+        return new ActionSlug(
+            ModuleName::fromString($moduleName),
+            ActionName::fromString($actionName)
+        );
     }
 }
