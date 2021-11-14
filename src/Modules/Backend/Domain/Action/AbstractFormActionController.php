6076b9710c58335b5efee9e506d79b2f7216c24e
Jelmer Prins <jelmer@pageon.be>
2021-11-15 00:41:42 +0100

    Easy form handling in the backend with theme rendering

---
+++ src/Modules/Backend/Domain/Action/AbstractFormActionController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace ForkCMS\Modules\Backend\Domain\Action;
+
+use Doctrine\ORM\EntityManagerInterface;
+use ForkCMS\Core\Domain\Header\Header;
+use Pageon\DoctrineDataGridBundle\DataGrid\DataGridFactory;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Twig\Environment;
+
+abstract class AbstractFormActionController extends AbstractActionController
+{
+    public function __construct(
+        protected DataGridFactory $dataGridFactory,
+        protected EntityManagerInterface $entityManager,
+        protected Environment $twig,
+        protected TranslatorInterface $translator,
+        protected Header $header,
+        protected RouterInterface $router,
+        protected FormFactoryInterface $formFactory,
+        protected MessageBusInterface $commandBus,
+    ) {
+        parent::__construct($dataGridFactory, $entityManager, $twig, $translator, $header, $router);
+    }
+
+    protected function execute(Request $request): void
+    {
+    }
+
+    public function getResponse(Request $request): Response
+    {
+        return $this->getFormResponse($request) ?? parent::getResponse($request);
+    }
+
+    abstract protected function getFormResponse(Request $request): ?Response;
+
+    /**
+     * @param null|callable(FormInterface): Response|FormInterface|null $defaultCallback
+     * @param null|callable(FormInterface): Response|FormInterface|null $validCallback
+     */
+    protected function handleForm(
+        Request $request,
+        string $formType,
+        callable $defaultCallback = null,
+        callable $validCallback = null,
+        ?RedirectResponse $redirectResponse = null,
+        object $formData = null,
+        array $formOptions = [],
+    ): Response|FormInterface|null {
+        $defaultCallback ??= function (FormInterface $form): ?FormInterface {
+            $this->assign('backend_form', $form->createView());
+
+            return null;
+        };
+        $validCallback ??= function (FormInterface $form) use ($redirectResponse): ?Response {
+            $this->commandBus->dispatch($form->getData());
+
+            return $redirectResponse;
+        };
+
+        $form = $this->formFactory->create($formType, $formData, [
+
+        ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $validCallback($form);
+        }
+
+        return $defaultCallback($form);
+    }
+}
