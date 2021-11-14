f7f6acce9333085839eddd0a7cc315ff422e59e1
Jelmer Prins <jelmer@pageon.be>
2021-11-15 00:43:56 +0100

    Implement user group index

--- src/Modules/Backend/Backend/Actions/GroupIndex.php
+++ src/Modules/Backend/Backend/Actions/GroupIndex.php
@@ -2,14 +2,23 @@
 
 namespace ForkCMS\Modules\Backend\Backend\Actions;
 
+use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
 use ForkCMS\Modules\Backend\Domain\Action\AbstractActionController;
+use ForkCMS\Modules\Backend\Domain\UserGroup\UserGroup;
 use Symfony\Component\HttpFoundation\Request;
 
 final class GroupIndex extends AbstractActionController
 {
     protected function execute(Request $request): void
     {
-        // TODO: Implement execute() method.
-        throw new \Exception('Method not implemented');
+        $this->assign(
+            'groupDataGrid',
+            $this->dataGridFactory->forEntity(UserGroup::class, static function (QueryBuilder $queryBuilder): void {
+                $queryBuilder
+                    ->leftJoin('UserGroup.users', 'Users', Join::WITH, 'Users.deleted = 0')
+                    ->addSelect('Users');
+            })
+        );
     }
 }
