dab7e766d17b07b5ffc34f6319f0bd3ea03dda80
Jelmer Prins <jelmer@pageon.be>
2021-12-06 23:01:41 +0100

    Placeholder for widget controller interface

---
+++ src/Modules/Backend/Domain/Widget/WidgetControllerInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ForkCMS\Modules\Backend\Domain\Widget;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+interface WidgetControllerInterface
+{
+    public function __invoke(Request $request): Response;
+}
