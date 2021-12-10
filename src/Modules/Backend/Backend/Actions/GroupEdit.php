81713114ae262c3b69f12a7976e5c1b940d84fc8
Jelmer Prins <jelmer@pageon.be>
2021-12-10 03:38:16 +0100

    Implement changing a user group

--- src/Modules/Backend/Backend/Actions/GroupEdit.php
+++ src/Modules/Backend/Backend/Actions/GroupEdit.php
@@ -2,11 +2,11 @@
 
 namespace ForkCMS\Modules\Backend\Backend\Actions;
 
-use Assert\Assert;
-use Assert\Assertion;
+use ForkCMS\Core\Backend\Domain\Form\DeleteType;
 use ForkCMS\Modules\Backend\Domain\Action\AbstractFormActionController;
+use ForkCMS\Modules\Backend\Domain\Action\ActionSlug;
+use ForkCMS\Modules\Backend\Domain\Action\ModuleAction;
 use ForkCMS\Modules\Backend\Domain\UserGroup\Command\ChangeUserGroup;
-use ForkCMS\Modules\Backend\Domain\UserGroup\Command\CreateUserGroup;
 use ForkCMS\Modules\Backend\Domain\UserGroup\UserGroup;
 use ForkCMS\Modules\Backend\Domain\UserGroup\UserGroupType;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -22,6 +22,14 @@ final class GroupEdit extends AbstractFormActionController
     {
         /** @var UserGroup $userGroup */
         $userGroup = $this->getEntityFromRequest($request, UserGroup::class);
+
+        $this->setBreadcrumbDetail($userGroup->getName());
+
+        $this->addDeleteForm(
+            ['id' => $userGroup->getId()],
+            ActionSlug::fromFQCN(GroupDelete::class)
+        );
+
         return $this->handleForm(
             request: $request,
             formType: UserGroupType::class,
