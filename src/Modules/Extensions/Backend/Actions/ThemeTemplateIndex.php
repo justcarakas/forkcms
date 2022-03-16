81e470faf4e7429aef105e4f4d8176ba62524f03
Jelmer Prins <jelmer@pageon.be>
2022-03-16 21:55:24 +0100

    Theme template overview

---
+++ src/Modules/Extensions/Backend/Actions/ThemeTemplateIndex.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace ForkCMS\Modules\Extensions\Backend\Actions;
+
+use Doctrine\ORM\QueryBuilder;
+use ForkCMS\Modules\Backend\Domain\Action\AbstractDataGridActionController;
+use ForkCMS\Modules\Extensions\Domain\Theme\Theme;
+use ForkCMS\Modules\Extensions\Domain\Theme\ThemeRepository;
+use ForkCMS\Modules\Extensions\Domain\ThemeTemplate\ThemeTemplate;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+final class ThemeTemplateIndex extends AbstractDataGridActionController
+{
+    protected function execute(Request $request): void
+    {
+        $theme = $this->getTheme($request);
+        $this->renderDataGrid(
+            ThemeTemplate::class,
+            static function (QueryBuilder $queryBuilder) use ($theme): void {
+                $queryBuilder->andWhere('ThemeTemplate.theme = :theme')->setParameter('theme', $theme);
+            }
+        );
+        $this->assign('selectedTheme', $theme);
+        $this->assign('themes', $this->getRepository(Theme::class)->findAll());
+    }
+
+    private function getTheme(Request $request): Theme
+    {
+        /** @var ThemeRepository $themeRepository */
+        $themeRepository = $this->getRepository(Theme::class);
+
+        try {
+            return $this->getEntityFromRequest($request, Theme::class);
+        } catch (NotFoundHttpException) {
+            return $themeRepository->findOneBy(['active' => true]);
+        }
+    }
+}
