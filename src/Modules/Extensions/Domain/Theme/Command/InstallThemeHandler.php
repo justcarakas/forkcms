4e9e39a141146e7c07191c42ff2a35939ec3e330
Jelmer Prins <jelmer@pageon.be>
2022-03-29 15:03:31 +0200

    Implement installing theme template default blocks

--- src/Modules/Extensions/Domain/Theme/Command/InstallThemeHandler.php
+++ src/Modules/Extensions/Domain/Theme/Command/InstallThemeHandler.php
@@ -3,22 +3,59 @@
 namespace ForkCMS\Modules\Extensions\Domain\Theme\Command;
 
 use ForkCMS\Core\Domain\MessageHandler\CommandHandlerInterface;
+use ForkCMS\Core\Domain\Settings\SettingsBag;
+use ForkCMS\Modules\Extensions\Domain\Module\ModuleName;
 use ForkCMS\Modules\Extensions\Domain\Theme\Event\ThemeInstalledEvent;
 use ForkCMS\Modules\Extensions\Domain\Theme\Theme;
 use ForkCMS\Modules\Extensions\Domain\Theme\ThemeRepository;
 use ForkCMS\Modules\Extensions\Domain\ThemeTemplate\Event\ThemeTemplateCreatedEvent;
+use ForkCMS\Modules\Extensions\Domain\ThemeTemplate\InstallableThemeTemplate;
+use ForkCMS\Modules\Frontend\Domain\Action\ActionName;
+use ForkCMS\Modules\Frontend\Domain\Block\BlockRepository;
+use ForkCMS\Modules\Frontend\Domain\Block\Type;
+use ForkCMS\Modules\Frontend\Domain\Widget\WidgetName;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 final class InstallThemeHandler implements CommandHandlerInterface
 {
     public function __construct(
         private readonly ThemeRepository $themeRepository,
-        private readonly EventDispatcherInterface $eventDispatcher
+        private readonly EventDispatcherInterface $eventDispatcher,
+        private readonly BlockRepository $blockRepository
     ) {
     }
 
     public function __invoke(InstallTheme $installTheme)
     {
+        foreach ($installTheme->theme->templates as $template) {
+            if (!$template instanceof InstallableThemeTemplate) {
+                continue;
+            }
+            $template->setPositions(
+                array_map(function (array $position): array {
+                    $blocks = [];
+                    foreach ($position['blocks'] as $block) {
+                        $blocks[] = $this->blockRepository->findUnique(
+                            ModuleName::fromString($block['@module']),
+                            match (Type::from($block['@type'])) {
+                                Type::ACTION => ActionName::fromString($block['@name']),
+                                Type::WIDGET => WidgetName::fromString($block['@name']),
+                            },
+                            new SettingsBag(
+                                array_filter(
+                                    $block,
+                                    static fn ($key) => !str_starts_with($key, '@'),
+                                    ARRAY_FILTER_USE_KEY
+                                )
+                            )
+                        )?->getId();
+                    }
+                    $position['blocks'] = array_filter($blocks, is_int(...));
+
+                    return $position;
+                }, $template->getPositions())
+            );
+        }
         $theme = Theme::fromDataTransferObject($installTheme->theme);
         $installTheme->theme->setTheme($theme);
         $this->themeRepository->save($theme);
--- src/Modules/Extensions/Domain/ThemeTemplate/InstallableThemeTemplate.php
+++ src/Modules/Extensions/Domain/ThemeTemplate/InstallableThemeTemplate.php
@@ -5,6 +5,8 @@ namespace ForkCMS\Modules\Extensions\Domain\ThemeTemplate;
 use SimpleXMLElement;
 use Symfony\Component\HtmlSanitizer\HtmlSanitizer;
 use Symfony\Component\HtmlSanitizer\HtmlSanitizerConfig;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Serializer;
 
 final class InstallableThemeTemplate extends ThemeTemplateDataTransferObject
 {
@@ -17,11 +19,31 @@ final class InstallableThemeTemplate extends ThemeTemplateDataTransferObject
 
         $themeTemplate = new self();
         $themeTemplate->name = (string) $template->attributes()->name;
-        $themeTemplate->path = (string) $template->attributes()->path;
+        $themeTemplate->path = str_replace(ThemeTemplate::PATH_DIRECTORY, '', $template->attributes()->path);
         $themeTemplate->active = true;
-        $format = $sanitiser->sanitize(str_replace(' ', '', trim($template->format)));
-        $themeTemplate->settings->set('format', $format);
-        $themeTemplate->settings->set('positions', array_values(self::getPositionsFromFormat($format)));
+        $layout = $sanitiser->sanitize(str_replace(' ', '', trim($template->layout)));
+        $themeTemplate->settings->set('layout', $layout);
+        $positions = [];
+        $serialiser = new Serializer([], [new XmlEncoder()]);
+        foreach ($template->positions as $xmlPositions) {
+            foreach ($xmlPositions->position as $xmlPosition) {
+                $blocks = [];
+                foreach ($xmlPosition->block as $xmlBlock) {
+                    $blocks[] = $serialiser->decode($xmlBlock->saveXML(), 'xml');
+                }
+                $positions[] = [
+                    'name' => (string) $xmlPosition->attributes()->name,
+                    'blocks' => $blocks,
+                ];
+            }
+        }
+        if (count($positions) === 0) {
+            $positions = array_map(
+                static fn (string $position): array => ['name' => $position],
+                ThemeTemplateDataTransferObject::getPositionsFromFormat($layout)
+            );
+        }
+        $themeTemplate->settings->set('positions', $positions);
 
         return $themeTemplate;
     }
