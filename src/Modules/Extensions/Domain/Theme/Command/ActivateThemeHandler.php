2a0047182afc05c69d41d176d0a435faacc15326
Jelmer Prins <jelmer@pageon.be>
2022-05-14 12:26:06 +0200

    Add empty theme

--- src/Modules/Extensions/Domain/Theme/Command/ActivateThemeHandler.php
+++ src/Modules/Extensions/Domain/Theme/Command/ActivateThemeHandler.php
@@ -2,6 +2,7 @@
 
 namespace ForkCMS\Modules\Extensions\Domain\Theme\Command;
 
+use ForkCMS\Core\Domain\Kernel\Event\ClearCacheEvent;
 use ForkCMS\Core\Domain\MessageHandler\CommandHandlerInterface;
 use ForkCMS\Modules\Extensions\Domain\Theme\Event\ThemeActivatedEvent;
 use ForkCMS\Modules\Extensions\Domain\Theme\ThemeRepository;
@@ -19,5 +20,6 @@ final class ActivateThemeHandler implements CommandHandlerInterface
     {
         $this->themeRepository->activateTheme($activateTheme->theme);
         $this->eventDispatcher->dispatch(new ThemeActivatedEvent($activateTheme->theme));
+        $this->eventDispatcher->dispatch(new ClearCacheEvent());
     }
 }
