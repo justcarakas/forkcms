8604fb98a424e919c2b66cf9a1aaff36a8d1e811
Jelmer Prins <jelmer@pageon.be>
2026-06-21 19:42:04 +0200

    Fix backend language based settings not loading because of Tabype changes

--- src/Modules/Internationalisation/Domain/ModuleSettings/Command/ChangeModuleSettings.php
+++ src/Modules/Internationalisation/Domain/ModuleSettings/Command/ChangeModuleSettings.php
@@ -2,10 +2,13 @@
 
 namespace ForkCMS\Modules\Internationalisation\Domain\ModuleSettings\Command;
 
+use ForkCMS\Core\Domain\Form\TabsType;
+use ForkCMS\Core\Domain\Form\TabType;
 use ForkCMS\Modules\Internationalisation\Domain\Locale\InstalledLocale;
 use ForkCMS\Modules\Internationalisation\Domain\Locale\InstalledLocaleDataTransferObject;
 use ForkCMS\Modules\Internationalisation\Domain\Locale\Locale;
 use RuntimeException;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 final class ChangeModuleSettings
 {
@@ -15,7 +18,7 @@ final class ChangeModuleSettings
     /** @var InstalledLocaleDataTransferObject[] */
     public array $installedLocales = [];
 
-    public function __construct(InstalledLocale ...$installedLocales)
+    public function __construct(private SluggerInterface $slugger, InstalledLocale ...$installedLocales)
     {
         foreach ($installedLocales as $installedLocale) {
             if ($installedLocale->isDefaultForWebsite()) {
@@ -24,7 +27,7 @@ final class ChangeModuleSettings
             if ($installedLocale->isDefaultForUser()) {
                 $this->defaultForUser = $installedLocale->getLocale();
             }
-            $tabKey = md5($installedLocale->getLocale()->asTranslatable());
+            $tabKey = TabsType::getTabNameForLabel($installedLocale->getLocale()->asTranslatable(), $slugger);
             $this->installedLocales[$tabKey] = new InstalledLocaleDataTransferObject($installedLocale);
         }
     }
