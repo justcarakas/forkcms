af67afb67f801d2e02467929ee3f84d55b0924f9
Jelmer Prins <jelmer@pageon.be>
2026-06-24 14:15:00 +0200

    Better error handling when the module xml isn't valid

--- src/Modules/Extensions/Domain/Module/ModuleInformation.php
+++ src/Modules/Extensions/Domain/Module/ModuleInformation.php
@@ -76,21 +76,31 @@ final class ModuleInformation
 
     public static function fromXML(string $xmlFilePath): self
     {
+        $invalidXML = new self(
+            ModuleName::fromString(basename(dirname($xmlFilePath))),
+            '?.?.?',
+            '',
+            [],
+            [],
+            new Messages([TranslationKey::error('InvalidXML')])
+        );
+
         try {
+            $xmlContents = file_get_contents($xmlFilePath);
+            if ($xmlContents === false) {
+                return $invalidXML;
+            }
+
             $moduleConfig = simplexml_load_string(
-                file_get_contents($xmlFilePath),
+                $xmlContents,
                 'SimpleXMLElement',
                 LIBXML_NOCDATA | LIBXML_NOERROR | LIBXML_NOWARNING
             );
+            if ($moduleConfig === false) {
+                return $invalidXML;
+            }
         } catch (Throwable) {
-            return new self(
-                ModuleName::fromString(basename(dirname($xmlFilePath))),
-                '?.?.?',
-                '',
-                [],
-                [],
-                new Messages([TranslationKey::error('InvalidXML')])
-            );
+            return $invalidXML;
         }
         $messages = new Messages();
         Requirements::fromXML($moduleConfig->requirements, $messages);
